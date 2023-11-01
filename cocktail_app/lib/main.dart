627afd6ba82aabd074a82c8a1169fd4fa9c2ebb1
Jun <j.zhao1@newcastle.ac.uk>
2023-11-01 10:28:04 +0000

    added title, colour theme and button

--- cocktail_app/lib/main.dart
+++ cocktail_app/lib/main.dart
@@ -12,7 +12,9 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: 'Poppins'),
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.teal)
+                .copyWith(secondary: Colors.yellow)),
         home: const Homepage());
   }
 }
--- cocktail_app/lib/pages/home.dart
+++ cocktail_app/lib/pages/home.dart
@@ -6,7 +6,24 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 2,
+        title: const Text(
+          '🍸 Gen A Drink 🍸',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: ElevatedButton(
+        // Generate random cocktail
+        onPressed: () => {},
+        style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 231, 226, 182),
+            elevation: 0.5),
+        child: const Text('Generate'),
+      )),
     );
   }
 }
